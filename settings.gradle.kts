0336bfe17dedb38023da7f4b1712011af3e80bb4
Blarc <jakob.malezic@gmail.com>
2026-02-21 15:46:18 +0100

    chore: add Foojay toolchain resolver plugin for java support in Gradle

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,6 @@
+// so Gradle can download JDK 21 automatically and resolve the javaCompiler toolchain during model import
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+}
+
 rootProject.name = "ai-commits-intellij-plugin"
